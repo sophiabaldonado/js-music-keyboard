a327a13f531813c1cd48bf05fb775b9d2bdc5c82
sophia <sophiabaldonado@gmail.com>
2016-06-29 14:01:39 -0700

    sound works with click, repeats;

--- noise.js
+++ noise.js
@@ -1,3 +1,14 @@
 $(document).ready( function() {
-  // your code here
-});
+  sounds = $("#audio_embeds")
+  container = $(".instrument")
+  buttons = container.children('button.note')
+
+  buttons.click(function(event) {
+    event.preventDefault()
+
+    var button = $(this)
+    id = this.getAttribute("id")
+    sounds.children("#" + id + "Audio")[0].load()
+    sounds.children("#" + id + "Audio")[0].play()
+  })
+})
